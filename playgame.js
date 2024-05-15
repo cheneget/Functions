cc4798917277b9d63278f80699e380a9f6776433
cheneget <paul.ndege@teach2give.com>
2024-05-15 17:41:54 +0300

    First Commit

--- playgame.js
+++ playgame.js
@@ -1,5 +1,6 @@
 function playRound(playerChoice) {
     const computerChoice = getComputerChoice();
+    const palyerChoice = getPlayerChoice();
     const outcome = determineWinner(playerChoice, computerChoice);
     displayResult(outcome, playerChoice, computerChoice);
 }
